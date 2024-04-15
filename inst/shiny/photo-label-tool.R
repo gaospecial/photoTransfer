93a22fea6fbbb9c46521d8e7eb6664e5b0e5f311
Chun-Hui Gao <gaospecial@gmail.com>
2024-04-15 22:15:19 +0800

    update version

--- inst/shiny/photo-label-tool.R
+++ inst/shiny/photo-label-tool.R
@@ -15,26 +15,41 @@ ui <- fluidPage(
       hr(),
       # 添加下载链接
       downloadLink("save", "下载标注"),
-      # 隐藏的输入，用于接收current_index的更新
-      tags$div(style = "display: none;", numericInput("current_index", "current_index", value = 1, min = 1))
     ),
 
     mainPanel(width = 6,
-      imageOutput("imageDisplay", click = "image_click"),
+      imageOutput("imageDisplay"),
       textOutput("current_image"),
       fluidRow(
         column(2, align = "left", actionButton("prev_photo", "上一张", icon = icon("arrow-left"))),
         column(2, align = "center", textInput("imageLabel", NULL, "")),
         column(2, align = "right", actionButton("next_photo", "下一张", icon = icon("arrow-right")))
-      )
+      ),
+      verbatimTextOutput("info") # 用于显示与DataTable相关变量的UI组件
     )
   )
 )
 
 server <- function(input, output, session) {
+
+  # browser()
+  reactlog()
+
+  example_annotation = data.frame(image_path = "www/images/P8H5.jpg", old_label = "", current_label = "")
+  output$dataTable <- renderDT(example_annotation, selection = "single", server = TRUE)
+
   # 存储图片数据和标注的反应性值
-  annotations <- reactiveVal(data.frame(image_path = "www/images/P8H5.jpg", old_label = "", current_label = ""))
-  current_index <- reactiveVal()
+  annotations <- reactiveVal(example_annotation, 'annotations')
+  current_index <- reactiveVal(1, 'index')
+
+  # 显示DataTable相关的输入值
+  output$info <- renderPrint({
+    str(list(
+      annotations = annotations(),
+      current_index = current_index(),
+      datatable_rows_selected = input$dataTable_rows_selected
+    ))
+  })
 
   # 处理文件上传并更新标注数据
   observeEvent(input$fileInput, {
@@ -43,38 +58,23 @@ server <- function(input, output, session) {
     user_data = read_csv(file$datapath, col_types = "c")
     colnames(user_data) = c("image_path","old_label")
     user_data$current_label = user_data$old_label
+
+    # 在UI中显示标注数据
+    output$dataTable <- renderDT(user_data, selection = "single", server = TRUE)
     annotations(user_data)
     current_index(1)
   })
 
-  # 在UI中显示标注数据
-  output$dataTable <- renderDT({
-    datatable(annotations(), selection = "none", escape = FALSE,
-        # 响应鼠标点击，更新 current_index 的值
-        callback = JS("
-            table.on('click', 'tr', function () {
-                var data = table.row(this).data();
-                if (data) {
-                    // 获取当前行的索引（注意：索引是从0开始的）
-                    var index = table.row(this).index();
-                    // 通过Shiny.setInputValue更新current_index
-                    Shiny.setInputValue('current_index', index + 1); // 索引+1以匹配R中的1-based索引
-                }
-            });
-        "),
-        # 响应 current_index 更新高亮的行
-        options = list(rowCallback = JS("
-                function(row, data, displayNum, displayIndex, dataIndex) {
-                  if (dataIndex === ", current_index(), " - 1) {
-                    $(row).addClass('selected');
-                  } else {
-                    $(row).removeClass('selected');
-                  }
-                }
-            "))
-    )
+  # 创建一个 datatable proxy
+  dataTableProxy <- dataTableProxy('dataTable')
+
+  # 使用选中的行更新 current_index
+  observeEvent(input$dataTable_rows_selected, {
+    selected_absolute_index = input$dataTable_rows_selected
+    current_index(selected_absolute_index)
   })
 
+  # 输出图片路径
   output$current_image = renderText({
     data = annotations()
     idx = current_index()
@@ -83,29 +83,32 @@ server <- function(input, output, session) {
 
   # 切换到上一张图片
   observeEvent(input$prev_photo, {
-    idx <- current_index()
-    if(idx > 1) {
-      current_index(idx - 1)
-      # 同步更新dataTable中加粗显示的行
+    idx = current_index()
+    indexes = input$dataTable_rows_all
+    if (is.null(indexes)) return()
+    pos = which(indexes == idx)
+    if (pos > 1) {
+      current_index(indexes[pos - 1])
     } else {
       # 如果是第一个元素，那么禁用 prev_photo 按钮
       disable("prev_photo")
     }
-    # 将 imageLabel 更新为 current_label
   })
 
   # 切换到下一张图片
   observeEvent(input$next_photo, {
-    data <- annotations()
-    idx <- current_index()
-    if(idx < nrow(data)) {
-      current_index(idx + 1)
-      # 同步更新dataTable中加粗显示的行
+    idx = current_index()
+    indexes = input$dataTable_rows_all
+    if (is.null(indexes)) return()
+    pos = which(indexes == idx)
+    if (pos < length(indexes)) {
+      current_index(indexes[idx + 1])
     } else {
       # 如果是最后一个元素，那么禁用 next_photo 按钮
       disable("next_photo")
     }
   })
+
 
   # 响应 index 变化，更新当前图片、dataTable 的显示和按钮状态
   observeEvent(current_index(), {
@@ -114,14 +117,20 @@ server <- function(input, output, session) {
     # 更新图片显示
     output$imageDisplay <- renderImage({
       imagePath <- data$image_path[[idx]]
-      # showNotification(imagePath)
       # 直接从磁盘路径加载图片
       list(src = imagePath, width = "400px")
     }, deleteFile = FALSE)
 
     # 更新 dataTable 状态，高亮显示 idx 的行
-    dataTableProxy <- dataTableProxy('dataTable')
-    selectRows(dataTableProxy, idx, select = TRUE)
+    selectRows(dataTableProxy, c(row = idx))
+
+    # 显示对应的页面
+    indexes = input$dataTable_rows_all
+    if (is.null(indexes)) return()
+    datatable_page_length = input$dataTable_state$length
+    targetRow = which(indexes == idx)
+    targetPage = ceiling(targetRow / datatable_page_length) # 向上取整得到页码
+    selectPage(dataTableProxy, targetPage)
 
     # 更新 imageLabel
     updateTextInput(session, inputId = "imageLabel", value = data$current_label[[idx]])
@@ -140,34 +149,15 @@ server <- function(input, output, session) {
   })
 
 
-  # 观察current_index的变化并更新
-  observeEvent(input$current_index, {
-    # 更新current_index的值
-    current_index(input$current_index)
-    # 可能还需要在这里添加其他代码，比如更新图片显示或按钮状态
-  })
-
   # 监听imageLabel输入并更新标注
   observeEvent(input$imageLabel, {
-    data <- annotations()
-    idx <- current_index()
-    data[idx, "current_label"] <- input$imageLabel
-    annotations(data) # 更新标注数据
-    current_index(idx)
-  })
-
-  # 当 annotation 更新时，同步更新 dataTable
-  observeEvent(annotations(), {
-    data = annotations()
     idx = current_index()
+    data <- annotations()
+    data[idx, "current_label"] = input$imageLabel
+    annotations(data) # 更新标注数据
 
-    # 用新数据替换旧数据
-    replaceData(dataTableProxy('dataTable'), data)
-
-    # 将 idx 对应的行加上 .selected 类
-    dataTableProxy <- dataTableProxy('dataTable')
-    selectRows(dataTableProxy, idx, select = TRUE)
-
+    # 同步更新 dataTable
+    replaceData(dataTableProxy, data, resetPaging = FALSE, clearSelection = "none")
   })
 
   # 处理标注保存逻辑
